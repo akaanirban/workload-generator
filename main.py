d0deaa056bdcdd71ff7ed8d5f3e7b00cb00590ba
akaanirban <anirban.dash@gmail.com>
2018-05-30 14:14:21 -0400

    made time command line argument

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from Generators.ImageDataGenerator import ImageDataGenerator
 import logging
-
+import sys
 logging.getLogger(__name__)
 
 # TODO: Use a config file for the init  configuration
 
 if __name__ == "__main__":
+    time = float(sys.argv[1])
     new_workload = ImageDataGenerator(src="/home/anirban/Pictures/First200", dest="/home/anirban/Pictures/test",
-                                      time_interval =.01, chunk_size=4, repeat_images=True)
+                                      time_interval =time, chunk_size=4, repeat_images=True)
     new_workload.generate()
